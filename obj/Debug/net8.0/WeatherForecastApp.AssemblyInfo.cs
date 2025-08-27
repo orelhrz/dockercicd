7043746e3a01e6cd2ba8b443597661dc05e474eb
Orel Hernandez ramirez <or20509471@wipro.com>
2025-08-27 10:42:52 -0700

    add deploy task

--- obj/Debug/net8.0/WeatherForecastApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherForecastApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecastApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404b20ae4fb76d8eee1f190792e4208686234779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf225806d56d5752c3814c24644e560e2bf09f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecastApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecastApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
